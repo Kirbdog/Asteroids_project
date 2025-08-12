70c02877eb73f9855e0ef5d9e07845b8b9afff94
Kirbdog <kirbdog315@gmail.com>
2025-08-12 13:51:09 -0500

    Added rotation right and left

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+
         screen.fill("black")
+        player1.update(dt)
         player1.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
