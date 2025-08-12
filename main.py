ce5cc17cdc06ade412ef6fbc5868f8093376f40c
KirbyC <kirbdog315@gmail.com>
2025-08-11 19:46:05 -0500

    Added triangle sprite player to the middle of the screen.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from constants import *
 import pygame
+from player import Player
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -10,7 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
